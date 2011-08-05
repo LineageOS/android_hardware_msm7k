b408e7568b37eed34e9bf23c276c722d39ff098b
Giulio Cervera <giulio.cervera@gmail.com>
2011-08-05 23:45:47 +0200

    libaudio-msm7x30: cleanup
    Change-Id: Ibef4d8c43e4a6d5754848f959fd56115b04ab77a

--- libaudio-msm7x30/AudioPolicyManager.h
+++ libaudio-msm7x30/AudioPolicyManager.h
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2009 The Android Open Source Project
- * Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2009, The Android Open-Source Project
+ * Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011, The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,14 +16,12 @@
  * limitations under the License.
  */
 
-
 #include <stdint.h>
 #include <sys/types.h>
 #include <utils/Timers.h>
 #include <utils/Errors.h>
 #include <utils/KeyedVector.h>
 #include <hardware_legacy/AudioPolicyManagerBase.h>
-
 
 namespace android {
 
